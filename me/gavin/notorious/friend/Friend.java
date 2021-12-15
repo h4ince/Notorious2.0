a063cb253008b61248af40d4863057b7ed26ca2a
Notme11 <78656895+Notme11@users.noreply.github.com>
2021-12-15 11:21:50 +0700

    Create Friend.java

---
+++ me/gavin/notorious/friend/Friend.java
@@ -0,0 +1,18 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package me.gavin.notorious.friend;
+
+public class Friend
+{
+    String name;
+    
+    public Friend(final String n) {
+        this.name = n;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
